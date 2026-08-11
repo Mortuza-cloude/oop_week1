c832f7930c5d3628f8f1645d57f9b9ac75e8299e
Mortuza Rasel <mortuza.net1@gmail.com>
2026-08-11 22:57:02 +0900

    Suica Card Balance Check
    Add Suica Card Balance Check

--- Suica Card Balance Check.py
+++ Suica Card Balance Check.py
@@ -1,6 +1,7 @@
 # A Suica card has a balance of 320 yen. 
 # The train fare is 410 yen. Write an if-else statement that prints "Insufficient balance"
 #  if the balance is less than the fare, otherwise print "You can board the train".
+#
 
 
 balance = 320
