f4b37d8bd3b46f992365bbebb625577e61183ab1
Mortuza-cloude <mortuza.net1@gmail.com>
2026-08-11 23:54:23 +0900

    Konbini Shopping List

---
+++ Name Formatter.py
@@ -0,0 +1,8 @@
+# Given full_name = " tanaka yuki ", use string 
+# methods to remove extra spaces and print it properly capitalized as "Tanaka Yuki".
+# Hint: look into .strip(), .title()
+
+
+full_name = " tanaka yuki  "
+full_name = full_name.strip().title()
+print(full_name)
