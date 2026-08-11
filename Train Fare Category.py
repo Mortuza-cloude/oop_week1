0b50f473e1d6a6a3ca722995d5124f3278403935
Mortuza Rasel <mortuza.net1@gmail.com>
2026-08-11 22:57:22 +0900

    Train Fare Category
    Add Train Fare Category

--- Train Fare Category.py
+++ Train Fare Category.py
@@ -1,5 +1,6 @@
 # Write an if-elif-else chain that takes an age variable and prints the fare category: "Child"
 # if age < 12, "Student" if 12–22, "Adult" if 23–64, and "Senior" if 65 or above.
+#
 
 
 age=int(input("Enter your age: "))
